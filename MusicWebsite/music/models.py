65a210369174658b46c8c325c4a8098ccf744072
Vasu Gupta <androfly.official@gmail.com>
2017-10-15 16:44:25 +0530

    Django Work

--- MusicWebsite/music/models.py
+++ MusicWebsite/music/models.py
@@ -10,7 +10,13 @@ class Album(models.Model) :
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.artist + '_' + self.album_title
+
 class Song(models.Model) :
     album = models.ForeignKey(Album , on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.artist + '_' + self.album_title
