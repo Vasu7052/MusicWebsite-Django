7af933a9707446c060dca14e785b9791aad4deda
Vasu Gupta <androfly.official@gmail.com>
2017-10-15 16:55:29 +0530

    Django Work

--- MusicWebsite/music/urls.py
+++ MusicWebsite/music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index , name="index")
+    # /music/
+    url(r'^$', views.index , name="index") ,
+
+    # /music/45/
+    url(r'^(?P<album_id>[0-9]+)/$' , views.detail , name="detail")
 ]
--- MusicWebsite/music/views.py
+++ MusicWebsite/music/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request) :
-    return HttpResponse("<h1>This is a music app homepage</h1>")
+    return HttpResponse("<h1>All albums will be listed here</h1>")
+
+
+def detail(request , album_id) :
+    return HttpResponse("<h1>Details for album id: " + str(album_id) + "</h1>")
