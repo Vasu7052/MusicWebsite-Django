1a25d855b8717800989b99bff7f2ef600189d141
Vasu Gupta <androfly.official@gmail.com>
2017-10-15 17:13:47 +0530

    Django Work

--- MusicWebsite/music/views.py
+++ MusicWebsite/music/views.py
@@ -17,8 +17,12 @@ def index(request) :
 
 def index(request) :
     all_albums = Album.objects.all()
-    template = loader.get_template("")
-    return HttpResponse()
+    template = loader.get_template("music/index.html")
+    # Create a dictionary ..
+    context = {
+        "all_albums" : all_albums
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request , album_id) :
     return HttpResponse("<h1>Details for album id: " + str(album_id) + "</h1>")
