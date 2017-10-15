5d2a506280588776bdb34eee44818a2769fe1681
Vasu Gupta <androfly.official@gmail.com>
2017-10-15 17:06:40 +0530

    Django Work

--- MusicWebsite/music/views.py
+++ MusicWebsite/music/views.py
@@ -3,6 +3,7 @@ from .models import Album
 
 # Create your views here.
 
+'''
 def index(request) :
     all_albums = Album.objects.all()
     html = ""
@@ -11,7 +12,16 @@ def index(request) :
         html += '<a href="'+url+'" > ' + f.album_title + "</a><br>"
 
     return HttpResponse(html)
+'''
 
+def index(request) :
+    all_albums = Album.objects.all()
+    html = ""
+    for f in all_albums :
+        url = "/music/" + str(f.id) + "/"
+        html += '<a href="'+url+'" > ' + f.album_title + "</a><br>"
+
+    return HttpResponse(html)
 
 def detail(request , album_id) :
     return HttpResponse("<h1>Details for album id: " + str(album_id) + "</h1>")
