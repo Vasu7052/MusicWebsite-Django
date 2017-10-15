602e88810bd8a0dfc96f66ecbf8a5b4cf8962a32
Vasu Gupta <androfly.official@gmail.com>
2017-10-15 17:08:13 +0530

    Django Work

--- MusicWebsite/music/views.py
+++ MusicWebsite/music/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from .models import Album
+from django.template import loader
 
 # Create your views here.
 
@@ -16,12 +17,8 @@ def index(request) :
 
 def index(request) :
     all_albums = Album.objects.all()
-    html = ""
-    for f in all_albums :
-        url = "/music/" + str(f.id) + "/"
-        html += '<a href="'+url+'" > ' + f.album_title + "</a><br>"
-
-    return HttpResponse(html)
+    template = loader.get_template("")
+    return HttpResponse()
 
 def detail(request , album_id) :
     return HttpResponse("<h1>Details for album id: " + str(album_id) + "</h1>")
