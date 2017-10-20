56003561020b3e075ee03d513c42a9c85f006492
Vasu Gupta <androfly.official@gmail.com>
2017-10-20 14:05:34 +0530

    Django Work

--- MusicWebsite/music/models.py
+++ MusicWebsite/music/models.py
@@ -8,7 +8,7 @@ class Album(models.Model) :
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField()
 
     def __str__(self):
         return self.artist + '_' + self.album_title
